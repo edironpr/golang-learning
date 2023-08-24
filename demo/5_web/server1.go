ca6a8397df9ca86ae5bad2f9c5014fe2e453da75
ed <zhougangdong97@gmail.com>
2023-08-24 23:23:32 +0800

    update

---
+++ demo/5_web/server1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler1)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler1(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
---
+++ demo/5_web/server2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var count int
+var mu sync.Mutex
+
+func main() {
+	http.HandleFunc("/", handler2)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler2(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	_, _ = fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	_, _ = fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
